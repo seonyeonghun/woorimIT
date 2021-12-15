665e7a1b29bb839389e354f56b67f83810e9c155
seon yeong hun <seonyeonghun@kakao.com>
2021-12-16 01:48:57 +0900

    소스코드 src로 분류

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // 모듈 최적화
-const app = require('../app')
+const app = require('../../app')
 const port = 3000
 app.listen(port, ()=>{
     console.log(`server is running at ${port}`)
